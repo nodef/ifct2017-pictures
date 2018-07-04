edaede2da2605feb406f50f6f334384b1da03eb2
Subhajit Sahu <wolfram77@gmail.com>
2018-07-04 12:59:22 +0530

    :snowman: add unpkg, jsdelivr links

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-function pictures(id) {
-  if((id[0]>='M' && id[0]<'P') || id[0]>='T') return null;
-  return `https://unpkg.com/@ifct2017/pictures@0.2.0/${id}.jpeg`;
+const path = require('path');
+function pictures(cod) {
+  if(cod[0]>='M' && cod[0]<'P') return null;
+  return path.join(__dirname, cod+'.jpeg');
 };
+function unpkg(cod) {
+  if(cod[0]>='M' && cod[0]<'P') return null;
+  return `https://unpkg.com/@ifct2017/pictures/assets/${cod}.jpeg`;
+};
+function jsdelivr(cod) {
+  if(cod[0]>='M' && cod[0]<'P') return null;
+  return `https://cdn.jsdelivr.net/npm/@ifct2017/pictures/assets/${cod}.jpeg`;
+};
+pictures.unpkg = unpkg;
+pictures.jsdelivr = jsdelivr;
 module.exports = pictures;
